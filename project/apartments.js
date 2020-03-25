f75694317fc6d673f3784d3a8241fddcf3b6826c
ANDER50N-STEVEN <35932883+ANDER50N-STEVEN@users.noreply.github.com>
2020-03-25 13:34:39 -0700

    slider, buttons
    added slider for price, still need to calculate filter.  Got buttons working independently.

--- project/apartments.js
+++ project/apartments.js
@@ -8,11 +8,12 @@ eqfeed_callback({
         "lon": -122.685043
       },
       "style": {
-        "bed": 1,
-        "bath": 1,
-        "feet": 611
+        "bed": [1],
+        "bath": [1],
+        "feet": [611]
       },
-      "price": 1660
+      "price": [1660],
+      "img": ["./img/MPA/out.jpg", "./img/MPA/kit.jpg"]
     },
     {
       "name": "Heartline Apartments",
@@ -22,11 +23,12 @@ eqfeed_callback({
         "lon": -122.694527
       },
       "style": {
-        "bed": 1,
-        "bath": 1,
-        "feet": 702
+        "bed": [1],
+        "bath": [2],
+        "feet": [702]
       },
-      "price": 2020
+      "price": [2020],
+      "img": ["./img/Heart/out.jpg", "./img/Heart/bath.jpg", "./img/Heart/kit.jpg", "./img/Heart/live.jpg"]
     },
     {
       "name": "Tanner Flats",
@@ -36,11 +38,12 @@ eqfeed_callback({
         "lon": -122.694618
       },
       "style": {
-        "bed": 1,
-        "bath": 1,
-        "feet": 568
+        "bed": [2],
+        "bath": [1],
+        "feet": [568]
       },
-      "price": 1438
+      "price": [1438],
+      "img": ["./img/Tanner/kit.jpg", "./img/Tanner/live.jpg"]
     },
     {
       "name": "",
@@ -50,11 +53,43 @@ eqfeed_callback({
         "lon": -122.685800
       },
       "style": {
-        "bed": 0,
-        "bath": 1,
-        "feet": 745
+        "bed": [0, 1, 2],
+        "bath": [1],
+        "feet": [745]
       },
-      "price": 1695
+      "price": [1695, 1874, 2121],
+      "img": ["./img/1400/out.jpg", "./img/1400/bed.jpg", "./img/1400/kit.jpg", "./img/1400/live.jpg"]
+    },
+{
+      "name": "Yard Luxary Apartments",
+      "address": "22 NE 2nd Ave, Portland, OR 97232",
+     "location": {
+        "lat": 45.532230,
+        "lon": -122.663689
+      },
+      "style": {
+        "bed": [0, 1, 2],
+        "bath": [1, 2],
+        "feet": [504, 591, 1042]
+      },
+      "price": [1560, 1740, 2632],
+      "img": ["./img/YLA/out.jpg", "./img/YLA/bed.jpg", "./img/YLA/kit.jpg", "./img/YLA/live.jpg"]
+    },
+    {
+      "name": "Cordelia",
+      "address": "777 NW 19th Ave, Portland, OR 97209",
+     "location": {
+        "lat": 45.528278,
+        "lon": -122.690948
+      },
+      "style": {
+        "bed": [1, 2],
+        "bath": [1, 2],
+        "feet": [622, 1014]
+      },
+      "price": [1451, 2585],
+      "img": ["./img/Cordelia/kit.jpg", "./img/Cordelia/live.jpg"]
     }
   ]
 })
+
--- project/main.js
+++ project/main.js
@@ -1,10 +1,9 @@
 var map;
-
+var set = null; 
+var active = false;
 function initMap() {
   
   map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 2,
-      center: new google.maps.LatLng(2.8,-187.3),
       mapTypeId: 'terrain'
     });
 
@@ -18,11 +17,13 @@ function initMap() {
       // Loop through the results array and place a marker for each
       // set of coordinates.
 
-window.eqfeed_callback = function(results) {
-   var bounds = new google.maps.LatLngBounds();
-  var set = results.apartment;
+function eqfeed_callback(results) {
+  var bounds = new google.maps.LatLngBounds();
+  if(set === null){
+    set = results.apartment;
+    localStorage.setItem('data', JSON.stringify(set));
+  }
   for (var i = 0; i < set.length; i++) {
-    
     var rental = set[i];
     var coords = rental.location;
     var latLng = new google.maps.LatLng(coords.lat,coords.lon);
@@ -32,15 +33,22 @@ window.eqfeed_callback = function(results) {
       });
     bounds.extend(marker.position);
 
+    
+
+        //  Info window Content
+
     var content = '<div id="content">'+
       '<h1 id="firstHeading" class="firstHeading">'+ rental.name + '</h1>'+
       '<div id="bodyContent">'+
-      '<div> Beds: ' + rental.style.bed + '</div>'+
-      '<div> Bathrooms: ' + rental.style.bath + '</div>' +
-      '<div> Price: $' + rental.price + '<div>' +
+      '<div><img src="' + rental.img[0] + '" width="100px" height="70px">' +
+      '<div> Beds: ' + print(rental.style.bed, ' ') + '</div>'+
+      '<div> Bathrooms: ' + print(rental.style.bath, ' ') + '</div>' +
+      '<div> Price:' + print(rental.price, " $") + '<div>' +
       '</div>'    
 
     var infowindow = new google.maps.InfoWindow()
+
+      //  Mousover and out to display and remove info window
 
     google.maps.event.addListener(marker,'mouseover', (function(marker,content,infowindow){ 
         return function() {
@@ -55,5 +63,154 @@ window.eqfeed_callback = function(results) {
     })(marker, content, infowindow));  
   }
 map.fitBounds(bounds);
+console.log(bounds);
+if(bounds.Za[0] == bounds.Za[1])
+ map.setZoom(map.getZoom()-2);
 }
-    
+
+    // Filter depending on user input.
+      
+function filter(c, filter) {
+  var header = document.getElementById(filter+"Btn");
+  var x = header.getElementsByClassName("btn");
+
+  // To cycle through buttons when clicked, first button pressed only if onload.
+
+  for (var i = 0; i < x.length; i++) {
+    x[i].addEventListener("click", function() {
+      var current = header.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      localStorage.setItem('previous', current);
+      if(!isEmpty(set)){
+      this.className += " active";
+      }
+      else
+       x[0].className += " active";
+    });
+
+  }
+    //  Calls filter functions
+set = JSON.parse(localStorage.getItem('data'));
+  switch(filter){
+    case "bed":
+
+      localStorage.setItem('rooms', c);
+      filterBed(c);
+      if(localStorage.getItem('bath') != null && set != null)
+        filterBath(localStorage.getItem('bath'));
+      break;
+    case "bath":
+      localStorage.setItem('bath', c);
+      filterBath(c);
+      if(localStorage.getItem('rooms') != null && set != null)
+        filterBed(localStorage.getItem('rooms'));
+      break;
+    case "price":
+      filterPrice(document.getElementById('minPrice').value, document.getElementById('maxPrice').value);
+      break;
+    case "feet":
+      filterFeet(c);
+      break;
+    default:
+      break;
+  }
+
+  // initMap();
+  if(isEmpty(set)){
+    window.alert("We could not find any with this criteria search again");
+    set = null;
+    if(active){
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        active = false;
+      }
+      localStorage.clear();
+  }
+   initMap()
+
+}
+
+function filterBed(beds){
+  if (beds == "all"){ 
+    localStorage.removeItem('rooms');
+    set =JSON.parse(localStorage.getItem('data'));
+    return;}
+  else{
+    for (var i = 0; i < set.length; i++) {
+      var temp = set[i].style.bed;
+      if(!inArray(beds, temp)){
+        set.splice(i,1);
+        i--;
+      }
+    }
+  }
+console.log(JSON.parse(localStorage.getItem('data')));
+}
+
+function filterBath(baths){
+if (baths == "all"){ 
+    localStorage.removeItem('bath');
+    set =JSON.parse(localStorage.getItem('data'));
+    return;}
+  for (var i = 0; i < set.length; i++) {
+    var temp = set[i].style.bath;
+      if(!inArray(baths, temp)){
+        set.splice(i,1);
+        i--;
+    }
+  }
+    console.log(set);
+}
+
+function filterPrice(min, max){ 
+  if(min == "")
+    min = 0;
+  if(max == "")
+    max =1000000000;
+  for (var i = 0; i < set.length; i++) {
+    for(var j = 0; j < set[i].price.length; j++){
+    if(set[i].price[j] < min || set[i].price[j] > max){
+      set.splice(i,1);
+      // i--;
+    }}
+  }
+  
+}
+
+function filterFeet(min, max){
+  for (var i = 0; i < set.length; i++) {
+    if(set[i].style.feet < min || set[i].style.feet > max){
+      set.splice(i,1);
+      i--;
+    }
+  }
+  
+}
+
+function print(array, space){
+  var string ="";
+    for(var j = 0; j < array.length; j++){
+      string += space + array[j];
+      if(j+1 != array.length)
+        string+= ","
+    }
+    return string;
+}
+
+function inArray(needle,haystack)
+{
+    var count=haystack.length;
+    for(var i=0;i<count;i++)
+    {
+        if(haystack[i]==needle){return true;}
+    }
+    return false;
+}
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
